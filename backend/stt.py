2f2c8a0711b955c3b8cd336612eb65c41aa6c5fa
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-07-29 19:59:33 +0100

    Successful test transcription

--- backend/stt.py
+++ backend/stt.py
@@ -1,8 +1,13 @@
 from faster_whisper import WhisperModel
 
-model = WhisperModel("small", compute_type="float32")
+model = WhisperModel("small", device='cpu', compute_type='int8')
 
-def transcribe_audio(file_path):
+def transcribe_audio(file_path: str):
     segments, _ = model.transcribe(file_path, language='ar', beam_size=5)
     transcript = " ".join([segment.text for segment in segments])
     return transcript
+
+
+if __name__ == '__main__':
+    text = transcribe_audio('backend/arabic.wav')
+    print('Transcription:', text)
