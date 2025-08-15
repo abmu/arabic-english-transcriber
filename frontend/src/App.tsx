b17e041993d737fa0095bc831873fd9e93278abb
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-08-15 15:57:03 +0100

    Test change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -146,7 +146,7 @@ function App() {
 
   return (
     <div className='flex flex-col items-center gap-5 min-h-screen bg-gray-900 text-white p-6'>
-      <h1 className='text-2xl font-bold'>Live Arabic-English Translator</h1>
+      <h1 className='text-2xl font-bold'>Live Arabic-English Transcriber</h1>
       <div>
         <label>
           <select onChange={handleLanguageDirectionChange} disabled={isRecording} className='bg-gray-800 text-white border border-gray-700 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50'>
